8f2885bde5f726641f59fe5ca2644c48e2b4ca19
Rob Rose <RobRoseKnows+Git@gmail.com>
2020-06-19 20:33:33 -0400

    Added some config.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,68 @@
-import ForgeUI, { render, Fragment, Macro, Text } from "@forge/ui";
+import ForgeUI, { render, Fragment, Macro, Text, ConfigForm, TextField, useConfig, useState, TextArea } from "@forge/ui";
+
+const STATE = {
+  INITIAL: 0,
+  SUCCESS: 1
+};
+
+async function doWebhook(url, method, mode, )
+
+const Config = () => {
+
+  return (
+    <ConfigForm>
+      <TextField 
+        label="Button Text"
+        name="buttonText"
+        defaultValue="Trigger Webhook" />
+      <TextField
+        label="Webhook URL"
+        name="webhookURL"
+        placeholder="https://httpbin.org/get"
+        isRequired={true} />
+      <TextArea
+        label="Webhook Body"
+        name="webhookBody"
+        isMonospaced={true} />
+      <Select 
+        label="Webhook Method"
+        name="webhookMethod">
+        <Option defaultSelected label="GET" value="GET" />
+        <Option label="PUT" value="PUT" />
+        <Option label="POST" value="POST" />
+        <Option label="DELETE" value="DELETE" />
+      </Select>
+      <Select 
+        label="Webhook Mode"
+        name="webhookMode">
+        <Option defaultSelected label="cors" value="cors" />
+        <Option label="no-cors" value="no-cors" />
+        <Option label="same-origin" value="same-origin" />
+      </Select>
+      <TextArea
+        label="Webhook Headers"
+        name="webhookHeaders"
+        isMonospaced={true}
+        defaultValue="{'Content-Type': 'application/json'}" />
+    </ConfigForm>
+  )
+}
 
 const App = () => {
+  const config = useConfig();
+  const [ state, setState ] = useState(STATE.INITIAL);
+  const [ error, setError ] = useState(null);
+
+  if (!config) {
+    return doNeedConfig();
+  }
+
+  switch(state) {
+    case STATE.INITIAL:
+      return doInitial();
+    case STATE.SUCCESS:
+  }
+
   return (
     <Fragment>
       <Text>Hello world!</Text>
@@ -8,8 +70,4 @@ const App = () => {
   );
 };
 
-export const run = render(
-  <Macro
-    app={<App />}
-  />
-);
+export const run = render(<Macro app={<App />} config={<Config />}/>);
